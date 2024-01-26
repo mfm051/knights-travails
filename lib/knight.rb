478da5f2fdf9b6c58bc9e6ce847c07b166252d91
mateus <mateusmatos051@gmail.com>
2024-01-26 14:55:27 -0300

    Removes unnecessary clause in Knight#possible_moves

--- lib/knight.rb
+++ lib/knight.rb
@@ -9,14 +9,14 @@ class Knight
   end
 
   def possible_moves(initial_position = @initial_position)
+    raise ArgumentError, 'argument must be coordinate [x, y]' unless initial_position in [Integer, Integer]
+
     x_coord = initial_position[0]
     y_coord = initial_position[1]
 
     ChessBoard.squares.filter do |square|
       x_final = square[0]
       y_final = square[1]
-
-      x_final <= 7 && y_final <= 7
 
       steps_x = (x_final - x_coord).abs
       steps_y = (y_final - y_coord).abs
--- spec/knight_spec.rb
+++ spec/knight_spec.rb
@@ -10,5 +10,18 @@ describe Knight do
       possible_moves = humble_knight.possible_moves
       expect(possible_moves).to contain_exactly([2, 5], [4, 5], [1, 4], [5, 4], [1, 2], [5, 2], [2, 1], [4, 1])
     end
+
+    context 'when initial position is not a coordinate' do
+      it 'returns an error' do
+        expect { humble_knight.possible_moves(%w[a b]) }.to raise_error ArgumentError
+      end
+    end
+
+    context 'when initial position is out of board' do
+      it 'return an empty list' do
+        possible_moves = humble_knight.possible_moves([9, 9])
+        expect(possible_moves).to be_empty
+      end
+    end
   end
 end
